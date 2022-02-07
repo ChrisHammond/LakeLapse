7bb205913ac0b535bf38ae9695bb6422ba1b471d
Oliver Hine <oliver.hine@gmail.com>
2022-02-07 15:49:18 -0500

    update intervals to match production build

--- src/ProgramSettings.cs
+++ src/ProgramSettings.cs
@@ -8,15 +8,30 @@ namespace OliverHine.LakeLapseBot
 
         public double latitude = double.Parse(ConfigurationManager.AppSettings["Latitude"]);
         public double longitude = double.Parse(ConfigurationManager.AppSettings["Longitude"]);
+        public string CameraJpgUrl = ConfigurationManager.AppSettings["CameraJpgUrl"];
+
+        public string savePath = ConfigurationManager.AppSettings["SavePath"];
+        public string savePathImage = ConfigurationManager.AppSettings["SavePathImage"];
+
+        public string TwitterConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"];
+        public string TwitterConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"];
+        public string TwitterAccessToken = ConfigurationManager.AppSettings["TwitterAccessToken"];
+        public string TwitterAccessSecret = ConfigurationManager.AppSettings["TwitterAccessSecret"];
+
+        public string TwitterTweet = ConfigurationManager.AppSettings["TwitterTweet"];
+
+        public string MontageFolderName = "DailySunrise";
 
         public DateTime date = DateTime.Today;
         public DateTime CurrentDateTime = DateTime.Now;
+        public int currentDayOfMonth = -1;
 
-        public int photodelayProcessing = 10 * 1000;
-        public int photodelayNight = 8 * 1000;
-        public int photodelayDay = 6 * 1000;
-        public int photodelaySunrise = 2 * 1000;
-        public int photodelaySunset = 4 * 1000;
+        //note: even numbers required due to ramping function stepping in 2s intervals. 
+        public int photodelayProcessing = 90 * 1000;
+        public int photodelayNight = 60 * 1000;
+        public int photodelayDay = 40 * 1000;
+        public int photodelaySunrise = 10 * 1000;
+        public int photodelaySunset = 20 * 1000;
 
         public int preSunrise = -75;
         public int postSunrise = 35;
@@ -32,21 +47,7 @@ namespace OliverHine.LakeLapseBot
         public Timer? theTimer;
 
 
-        public string CameraJpgUrl = ConfigurationManager.AppSettings["CameraJpgUrl"];
-
-        public string savePath = ConfigurationManager.AppSettings["SavePath"];
-        public string savePathImage = ConfigurationManager.AppSettings["SavePathImage"];
-
-        public string TwitterConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"];
-        public string TwitterConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"];
-        public string TwitterAccessToken = ConfigurationManager.AppSettings["TwitterAccessToken"];
-        public string TwitterAccessSecret = ConfigurationManager.AppSettings["TwitterAccessSecret"];
-
-        public string TwitterTweet = ConfigurationManager.AppSettings["TwitterTweet"];
-
-        public string MontageFolderName = "DailySunrise";
-
-        public int currentDayOfMonth = -1;
+        
 
         public DateTime sunrise;
         public DateTime sunset;
